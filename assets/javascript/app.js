0d7e50c231f00abb5fefc58653f2bb6ea55c8561
Liz Leaverton <Liz Leaverton>
2017-07-30 20:09:54 -0500

    functional, next arrival time fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,7 @@ var config = {
     databaseURL: "https://trainscheduler-53177.firebaseio.com",
     projectId: "trainscheduler-53177",
     storageBucket: "",
+    //storage?
     messagingSenderId: "320427435073"
 };
 firebase.initializeApp(config);
@@ -12,43 +13,69 @@ firebase.initializeApp(config);
 //setup database variables and newTrain object
 var database = firebase.database();
 var trainRef = database.ref("/trains");
-var newTrain = {};
 
 //On submit button click, get values from the input fields and push those values to database
-$("#submitButton").on("click", function() {
+$("#submitButton").on("click", function(event) {
     console.log("click");
     event.preventDefault();
-    newTrain.name = $("#trainNameInput").val().trim();
-    newTrain.destination = $("#destinationInput").val().trim();
-    newTrain.startTime = $("#timeInput").val().trim();
-    newTrain.frequency = $("#frequencyInput").val().trim();
+    var trName = $("#trainNameInput").val().trim();
+    var trDestination = $("#destinationInput").val().trim();
+    var trStartTime = $("#timeInput").val().trim();
+    var trFrequency = $("#frequencyInput").val().trim();
+    
+
+    var newTrain = {
+    	name: trName,
+    	destination:trDestination,
+    	startTime:trStartTime,
+    	frequency:trFrequency
+    };
     console.log(newTrain);
 
     trainRef.push(newTrain);
 
-    trainRef.on("child_added", function(childSnapshot) {
-        console.log(childSnapshot.val());
-        //diff between current time and first time in minutes
-        //divide diff in minutes by frequency in minutes - remainder gives us amount of time into next frequency interval that has already passed
-        //frequency minus remainder = how many minutes away
-        //add how many minutes away to current time to get next train time. 
-
-        var startTimeConverted = moment(newTrain.startTime, "hh:mm").subtract(1, "years");
-        console.log(startTimeConverted);
-        var diffTime = moment().diff(moment(startTimeConverted),"minutes");
-        console.log(diffTime);
-        var tRemainder = diffTime%newTrain.frequency;
-        console.log(tRemainder);
-        var tMinutesTilTrain = newTrain.frequency-tRemainder;
-        console.log(tMinutesTilTrain);
-        var nextTrain = moment().add(tMinutesTilTrain,"minutes");
-        console.log(nextTrain);
-        var newRow = $('<tr><td>' + childSnapshot.val().name + '</td><td>' + childSnapshot.val().destination + '</td><td>' + childSnapshot.val().frequency + '</td><td>'+nextTrain+'</td><td>'+tMinutesTilTrain+'</td>');
-        $('table.trainTable').append(newRow);
-    });
-
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#timeInput").val("");
+    $("#frequencyInput").val("");
 
 });
+
+trainRef.on("child_added", function(childSnapshot, prevChildKey) {
+    console.log(childSnapshot.val());
+
+    var trName = childSnapshot.val().name;
+    var trDestination = childSnapshot.val().destination;
+    var trStartTime = childSnapshot.val().startTime;
+    var trFrequency = childSnapshot.val().frequency;
+    
+
+    //diff between current time and first time in minutes
+    //divide diff in minutes by frequency in minutes - remainder gives us amount of time into next frequency interval that has already passed
+    //frequency minus remainder = how many minutes away
+    //add how many minutes away to current time to get next train time. 
+
+    var startTimeConverted = moment(trStartTime, "hh:mm").subtract(1, "years");
+    console.log(startTimeConverted);
+
+    var diffTime = moment().diff(moment(startTimeConverted), "minutes");
+    console.log(diffTime);
+
+    var tRemainder = diffTime % trFrequency;
+    console.log(tRemainder);
+
+    var tMinutesTilTrain = trFrequency - tRemainder;
+    console.log(tMinutesTilTrain);
+
+    var nextTrain = moment().add(tMinutesTilTrain, "minutes").format("hh:mm");
+    console.log(nextTrain);
+
+    var newRow = $('<tr><td>' + trName + '</td><td>' + trDestination + '</td><td>' + trFrequency + '</td><td>' + nextTrain + '</td><td>' + tMinutesTilTrain + '</td>');
+    $('table.trainTable').append(newRow);
+});
+
+
+
 
 
 
